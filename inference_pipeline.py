085bf1ded1356560dd5b1438a739ea3415a6b76f
MarieGotthardt <45031262+MarieGotthardt@users.noreply.github.com>
2023-12-21 15:54:58 +0100

    Update inference_pipeline.py

--- inference_pipeline.py
+++ inference_pipeline.py
@@ -6,53 +6,9 @@ from openai import OpenAI
 import requests
 import os
 from ast import literal_eval
-#from graph_generation import *
+from graph_generation import *
 import numpy as np
 import matplotlib.pyplot as plt
-
-def get_sentiment_history():
-    project = hopsworks.login()
-    fs = project.get_feature_store()
-
-    most_positive_fg = fs.get_feature_group(name="articles_most_positive", version=3)
-    most_positive_df = most_positive_fg.read()
-    most_positive_sentiment = most_positive_df["sentiment"].values
-    most_positive_date = most_positive_df["pubdate"].values
-    average_sentiment = most_positive_df["avg_sentiment"].values
-    return most_positive_date, most_positive_sentiment, average_sentiment
-
-
-def plot_most_positive_timeline(most_positive_sentiment, most_positive_date, n):
-    if len(most_positive_date) > n:
-        most_positive_date = most_positive_date[-n:]
-        most_positive_sentiment = most_positive_sentiment[-n:]
-    else:
-        n = len(most_positive_date)
-
-    plt.plot(most_positive_date, most_positive_sentiment, '--co')
-    plt.xlabel("Date")
-    plt.ylabel("Sentiment Rating")
-    plt.ylim(-1, 1.1)
-    plt.title(f"Sentiment Ratings of Most Positive Articles for the Past {n} Days ")
-    plt.savefig('./most_positive_timeline.png')
-    plt.show()
-
-
-# Plot historical data for the average sentiment
-def plot_average_sentiment_timeline(average_sentiment, most_positive_date, n):
-    if len(most_positive_date) > n:
-        most_positive_date = most_positive_date[-n:]
-        average_sentiment = average_sentiment[-n:]
-    else:
-        n = len(most_positive_date)
-
-    plt.plot(most_positive_date, average_sentiment, '--om')
-    plt.xlabel("Date")
-    plt.ylabel("Sentiment Rating")
-    plt.ylim(-1, 1.1)
-    plt.title(f"Average Sentiment Ratings of the Past {n} Days ")
-    plt.savefig('./average_sentiment_timeline.png')
-    plt.show()
 
 
 def format_sentiment(sentiment):
@@ -117,7 +73,7 @@ def main():
     dataset_api.upload("./most_positive_timeline.png", "Resources/images", overwrite=True)
 
     # Plot average sentiment timeline and upload it to hopsworks
-    plot_most_positive_timeline(average_sentiment, most_positive_date, n=5)
+    plot_average_sentiment_timeline(average_sentiment, most_positive_date, n=5)
     dataset_api.upload("./average_sentiment_timeline.png", "Resources/images", overwrite=True)
 
     """
