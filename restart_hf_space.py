6a757c2b680277ea9fd004d3923f2e85352f9651
MarieGotthardt <45031262+MarieGotthardt@users.noreply.github.com>
2023-12-30 13:02:03 +0100

    removed the token as argument for the restart fct call

--- restart_hf_space.py
+++ restart_hf_space.py
@@ -4,7 +4,7 @@ from huggingface_hub import HfApi
 def main():
     hf_token = os.environ["HF_API_KEY"]
     HFAPI = HfApi(token=hf_token)
-    HFAPI.restart_space(repo_id="DelightNews/the-delight-dispatch-demo", token=hf_token)
+    HFAPI.restart_space(repo_id="DelightNews/the-delight-dispatch-demo")
 
 if __name__ == "__main__":
     main()
