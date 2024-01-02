e34478f4dec8044e133468d451aafac09a6efec2
marie <mariegotthardt224@gmail.com>
2024-01-02 19:59:04 +0100

    changed graph appearance to be more wes anderson like

--- graph_generation.py
+++ graph_generation.py
@@ -4,6 +4,10 @@ import hopsworks
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['text.color'] = 'black'
+mpl.rc('font',family='Courier New')
 
 def get_sentiment_history():
     project = hopsworks.login()
@@ -24,11 +28,16 @@ def plot_most_positive_timeline(most_positive_sentiment, most_positive_date, n):
     else:
         n = len(most_positive_date)
 
-    plt.plot(most_positive_date, most_positive_sentiment, '--co')
-    plt.xlabel("Date")
-    plt.ylabel("Sentiment Rating")
+    plt.plot(most_positive_date, most_positive_sentiment, '--o', color="teal", label="Most Positive Sentiment")
+    plt.xlabel("Date", fontname="Courier New", fontsize=12)
+    plt.xticks(fontname="Courier New", fontsize=10)
+    plt.ylabel("Sentiment Rating", fontname="Courier New", fontsize=12)
     plt.ylim(-1, 1.1)
-    plt.title(f"Sentiment Ratings of Most Positive Articles for the Past {n} Days ")
+    plt.yticks(fontname="Courier New", fontsize=10)
+    plt.axhline(y=0, xmin=0, xmax=len(most_positive_date), color='gray', linestyle='--', label="Baseline")
+    plt.title(f"Sentiment Ratings of Most Positive Articles \n for the Past {n} Days ", fontname="Courier New",
+              fontsize=12)
+    plt.legend(loc="lower right")
     plt.savefig('./most_positive_timeline.png')
     plt.show()
     plt.close()
@@ -42,11 +51,15 @@ def plot_average_sentiment_timeline(average_sentiment, most_positive_date, n):
     else:
         n = len(most_positive_date)
 
-    plt.plot(most_positive_date, average_sentiment, '--om')
-    plt.xlabel("Date")
-    plt.ylabel("Sentiment Rating")
+    plt.plot(most_positive_date, average_sentiment, '--o', color="darkred", label="Average Sentiment")
+    plt.xlabel("Date", fontname="Courier New", fontsize=12)
+    plt.xticks(fontname="Courier New", fontsize=10)
+    plt.ylabel("Sentiment Rating", fontname="Courier New", fontsize=12)
     plt.ylim(-1, 1.1)
-    plt.title(f"Average Sentiment Ratings of the Past {n} Days ")
+    plt.yticks(fontname="Courier New", fontsize=10)
+    plt.axhline(y=0, xmin=0, xmax=len(most_positive_date), color='gray', linestyle='--', label="Baseline")
+    plt.title(f"Average Sentiment Ratings of the Past {n} Days ", fontname="Courier New", fontsize=12)
+    plt.legend(loc="lower right")
     plt.savefig('./average_sentiment_timeline.png')
     plt.show()
     plt.close()
